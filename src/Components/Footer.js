1abdeec54678aa7ecb05d56d28683d6391617b83
rbqueiroz13 <grupopp33@gmail.com>
2023-04-20 12:28:17 -0300

    Carrinho e Footer atualizados!

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
     <div className="container text-center text-md-left">
         <div className="row text-center text-md-left">
             <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h5 className="text-uppercase mb-4 font-weight-bold text-dark">Empyrean Supplements</h5>
+                <h5 className="text-uppercase mb-4 font-weight-bold text-dark">QUEM SOMOS?</h5>
                 <p className="text-dark">Bem-vindo à sua loja! Como uma apaixonada entusiasta do fitness, nosssa empresa tem o prazer de ajudar você a encontrar os suplementos e acessórios perfeitos para apoiar seus objetivos!</p>
             </div>
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                 <h5 className="text-uppercase mb-4 font-weight-bold text-dark">Produtos</h5>
                 <p><a href="#" className="text-dark">Suplementos</a></p>
-                <p><a href="#" className="text-dark">acessórios</a></p>
+                <p><a href="#" className="text-dark">Acessórios</a></p>
                 <p><a href="#" className="text-dark">Moda esportiva</a></p>
             </div>
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
@@ -23,9 +23,9 @@ function Footer() {
             </div>
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h5 className="text-uppercase mb-4 font-weight-bold text-dark">Contato</h5>
-                <p><a href="mailto:agentavery@sample.com" className="text-dark">Email da empresa</a></p>
-                <p><a href="https://github.com/EmpyreanSupplements" className="text-dark">Github</a></p>
-                <p><a href="#" className="text-dark">Desenvolvedores</a></p>
+                <p><a href="mailto:agentavery@sample.com" className="text-dark"> <img src="https://img.icons8.com/fluency/48/null/apple-mail.png" width="50px"/>Email da empresa</a></p>
+                <p><a href="https://github.com/EmpyreanSupplements" className="text-dark"> <img src="https://img.icons8.com/fluency/48/null/github.png" width="50px"/>Github</a></p>
+                <p><a href="#" className="text-dark"> <img src="developers.png" width="40px"/>Desenvolvedores</a></p>
             </div>
         </div>
 
--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -12,7 +12,7 @@ function NavbarComp() {
         {['md'].map((expand) => (
           <Navbar key={expand} variant={"dark"} expand={expand} className="Nav">
             <Container fluid>
-              <img src="logoemgif.gif" alt='logo' className='logo'/>
+              <img src="logoemgif.gif" alt='logo' className='logo' width="50px" height="40px"/>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-${expand}`}
@@ -28,7 +28,7 @@ function NavbarComp() {
                   <Nav className="justify-content-end flex-grow-1 pe-3">
                     <Nav.Link className='c' href="/">Home</Nav.Link>
                     <Nav.Link className='c' href="/Carrinho">Carrinho</Nav.Link>
-                    <Nav.Link className='c' href="/Login">Entrar</Nav.Link>
+                    <Nav.Link className='c' href="/Cadastro">Cadastre-se</Nav.Link>
                   </Nav>
                   <Form className="d-flex">
                     <Form.Control
@@ -37,7 +37,9 @@ function NavbarComp() {
                       className="me-2"
                       aria-label="Search"
                     />
-                    <Button className='b'>Search</Button>
+                    <Button className='b' padding-bottom="20%" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg></Button>
                     
                   </Form>
                 </Offcanvas.Body>
